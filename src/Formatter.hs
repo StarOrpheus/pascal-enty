dde266feb682f65cddad68de54a317702bb5a556
starorpheus <ellesterate@gmail.com>
2020-10-16 17:43:05 +0300

    Finished PASTProgram rendering

--- src/Formatter.hs
+++ src/Formatter.hs
@@ -123,16 +123,93 @@ instance Renderable PASTProgramHeading where
             _ -> error "Unexpected program heading: Too many identifiers"
         ++ [""]
 
+appendLastLine :: String -> [String] -> [String]
+appendLastLine _ [] = []
+appendLastLine str lst = init lst ++ [last lst ++ str]
+
+appendSemi :: [String] -> [String]
+appendSemi = appendLastLine ";"
+
 renderStatementSeq :: [PASTStatement] -> [String]
 renderStatementSeq [] = []
-renderStatementSeq [a] = render a
-renderStatementSeq (a : as) = undefined -- TODO
+renderStatementSeq lst = renderStListImpl [] lst
+    where
+        renderStListImpl :: [String] -> [PASTStatement] -> [String]
+        renderStListImpl acc [] = acc
+        renderStListImpl acc (a : as) = renderStListImpl (appendSemi acc ++ render a) as
+
+renderIndented :: PASTStatement -> [String]
+renderIndented obj = case obj of
+    PASTCompoundStatement _         -> render obj
+    PASTEmptyStatement              -> render obj
+    PASTAssignStatement _ _         -> indent $ render obj
+    PASTProcedureStatement _ _      -> indent $ render obj
+    PASTConditionalStatement {}     -> indent $ render obj
+    PASTWhileStatement _ _          -> indent $ render obj
+    PASTForStatement {}             -> indent $ render obj
 
 instance Renderable PASTStatement where
     render (PASTCompoundStatement ss) =
         ["begin"] ++ indent (renderStatementSeq ss) ++ ["end"]
     render (PASTAssignStatement var expr) =
         [inplaceRender var ++ " := " ++ inplaceRender expr]
+    render (PASTProcedureStatement name args) =
+        if null args
+            then [name]
+            else [name ++ "(" ++ joinRenderables args ", " ++ ")"]
+    render PASTEmptyStatement = []
+    render (PASTConditionalStatement cond stmnt1 stmnt2) =
+        let ifPart = ("if (" ++ inplaceRender cond ++ ") then") : renderIndented stmnt1 in
+            case stmnt2 of
+                Nothing -> ifPart
+                Just st -> ifPart ++ ["else"] ++ renderIndented st
+    render (PASTWhileStatement cond stmt) =
+        ("while (" ++ inplaceRender cond ++ ") do") : renderIndented stmt
+    render (PASTForStatement varName range stmt) =
+        ("for " ++ varName ++ " := " ++ inplaceRender range ++ " do") : renderIndented stmt
+
+instance Renderable PASTFunctionalDecl where
+    render (PASTDeclFunction funcName resultType paramList varList block) =
+        let params = joinRenderables paramList ", " in
+            let varParams = if null paramList then "" else "; var " ++ joinRenderables varList ", " in
+        ("function " ++ funcName ++ "(" ++ params ++ varParams ++ "): " ++ inplaceRender resultType ++ ";") : appendSemi (render block)
+    render (PASTDeclProcedure funcName paramList varList block) =
+        let params = joinRenderables paramList ", " in
+            let paramsSeparator = if null paramList then "" else "; " in
+                let varParams = paramsSeparator ++ "var " ++ joinRenderables varList ", " in
+        ("procedure " ++ funcName ++ "(" ++ params ++ varParams ++ ");") : appendSemi (render block)
+
+instance Renderable PASTProgramBlock where
+    render (PASTProgramBlock constDecls varDecls funDecls statement) =
+        prepConstDecls constDecls
+     ++ prepVarDecls varDecls
+     ++ prepFunDecls funDecls
+     ++ render statement
+        where
+            prepConstDecls :: [PASTDeclConst] -> [String]
+            prepConstDecls [] = []
+            prepConstDecls (x : xs) =
+                ("const   " ++ inplaceRender x ++ ";") : indent (indent (fmap ((++ ";") . inplaceRender) xs)) ++ [""]
+
+            prepVarDecls :: [PASTDeclVar] -> [String]
+            prepVarDecls [] = []
+            prepVarDecls (x : xs) =
+                ("var " ++ inplaceRender x ++ ";") : indent (fmap ((++ ";") . inplaceRender) xs) ++ [""]
+
+            joinFunDecls = foldl' foldFunc []
+                where
+                    foldFunc acc newVal =
+                        let prefix = if null acc then acc else acc ++ [] in
+                            prefix ++ render newVal ++ [""]
+
+            prepFunDecls :: [PASTFunctionalDecl] -> [String]
+            prepFunDecls [] = []
+            prepFunDecls fns = joinFunDecls fns
+
+instance Renderable PASTProgram where
+    render (PASTProgram header block) = emplaceDot $ render header ++ render block
+        where
+            emplaceDot = appendLastLine "."
 
 pascalFormat :: PASTProgram -> [String]
-pascalFormat program = undefined
+pascalFormat = render
