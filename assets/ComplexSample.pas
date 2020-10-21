012a251a82b32f0d931fb5310c8103f35ae6301b
starorpheus <ellesterate@gmail.com>
2020-10-21 17:13:23 +0300

    Updated ComplexSample.pas

--- assets/ComplexSample.pas
+++ assets/ComplexSample.pas
@@ -1,4 +1,4 @@
-{source: http://sandbox.mc.edu/~bennet/cs404/doc/sort_pas.html}
+{I use (very) modified source from http://sandbox.mc.edu/~bennet/cs404/doc/sort_pas.html}
 
 PROGRAM Sort;
 const kek = 5;
@@ -7,20 +7,32 @@ VAR
     i, j, tmp, size: integer;
     arr: ARRAY [1..50] OF Integer;
 
+{ Procedural + IO sample}
 PROCEDURE ReadArr;
+    var tmp : integer;
 BEGIN
+    tmp := 42;
 	readln(size);
+    writeln('Size == ', size);
     for i := 1 to size do
-        read(arr[i]);
+    BEGIN
+        Readln(arr[i]);
+    end;
 END;
 
+{ Functional sample }
 function wrongSum(a, b : integer): integer;
 BEGIN
-    getSum := kek + a + b;
+    wrongSum := kek + a + b;
 END;
 
 BEGIN
+    tmp := 43;
     ReadArr;
+    writeln('Size after procedure call = ', size);
+
+    { State rollback example, global variable not affected by function call }
+    assert(tmp = 43);
 
     FOR i := size - 1 DOWNTO 1 DO
         FOR j := 1 TO i DO
@@ -30,9 +42,9 @@ BEGIN
                 arr[j + 1] := tmp;
             END;
 
-	writeln('Size: ', size);
     FOR i := 1 TO size DO
-        write(arr[i], ' ');
-    tmp := wrongSum(42, 16);
-    writeln('Wrong sum = ', tmp);
+        Write(arr[i], ' ');
+    writeln;
+
+    Writeln('Wrong sum = ', wrongSum(42, 16));
 END.
